dc77bdc9521da746061ff5f22993f5cc547866d5
CesarGarciaCabrera <cesargarciacabreratec@gmail.com>
2022-11-01 14:25:06 -0600

    Ejercicio variables-2.js listo, declaramos e inicializamos variables

---
+++ variables-2.js
@@ -0,0 +1,16 @@
+var nombre = "César García"
+
+//declarar
+var edad;
+
+//inicializar
+edad = 23;
+
+//Declarando e inicializando una variable
+var elementos = ["Computadora", "Celular"];
+// símbolo = asignar un valor
+elementos[0];
+
+var persona = { nombre: "César García", edad: 23} //generar un objeto que va a estar guardado en una variable
+
+persona;
