6aefcb644510a1c9b40fc0d5a275c924adf6f58a
CesarGarciaCabrera <cesargarciacabreratec@gmail.com>
2022-11-01 11:59:52 -0600

    Subí el ejercicio valores-1 donde vienen los tipos de datos de js

---
+++ valores-1.js
@@ -0,0 +1,11 @@
+//Tipos de datos primitivos
+40 //numero
+"César García Cabrera" //String
+true //boolean
+false //boolean
+
+null
+undefined
+
+[1,2,3] //Objeto
+{user: "César García Cabrera"} //Objeto
